8f89616e9c3ff347d3d357ed1dc62931d2858bc5
Vitor Oliveira <127554215+Vitorfran@users.noreply.github.com>
2024-08-25 11:22:03 -0300

    alteraçao usuario

--- usuario.java
+++ usuario.java
@@ -1,3 +1,30 @@
+
 public class usuario {
-    
+    public static void main(String[] args) {
+        SmartTv smartTv = new SmartTv();
+        smartTv.diminuirvolume();
+        smartTv.diminuirvolume();
+        smartTv.diminuirvolume();
+        smartTv.aumentarvolume();
+
+        System.out.println("Canal: " + smartTv.canal);
+        
+        smartTv.mudarCanal(13);
+        System.out.println("Canal: " + smartTv.canal);
+
+
+        System.out.println("TV ligada: " + smartTv.ligada);
+        System.out.println("Canal: " + smartTv.canal);
+        System.out.println("Volume: " + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println(" Novo status - TV ligada? : " + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println(" Novo status - TV ligada? : " + smartTv.ligada);
+
+
+
+
+    }
 }
